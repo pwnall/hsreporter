e0b0a905ed0efd79be10d2dedd81f4b9381b13e6
Victor Costan <costan@gmail.com>
2015-06-18 21:21:58 -0400

    More solid path detection on Windows.

--- reporter/paths.go
+++ reporter/paths.go
@@ -3,39 +3,51 @@ package reporter
 import (
   "os"
   "path/filepath"
-  "runtime"
 )
 
 // DefaultConfigFile returns the path to Hearthstone's logging config file.
 //
 // It returns the expected file path, assuming a standard game installation.
 func DefaultConfigFile() string {
-  var configRoot string
-  if runtime.GOOS == "windows" {
-    configRoot = os.Getenv("LOCALAPPDATA")
-  } else {
-    // OSX.
-    configRoot = filepath.Join(os.Getenv("HOME"), "Library", "Preferences")
+  // Windows attempt.
+  localAppData := os.Getenv("LOCALAPPDATA")
+  _, err := os.Stat(filepath.Join(localAppData, "Blizzard", "Hearthstone"))
+  if localAppData != "" && err == nil {
+    return filepath.Join(localAppData, "Blizzard", "Hearthstone", "log.config")
   }
-  return filepath.Join(configRoot, "Blizzard", "Hearthstone", "log.config")
+
+  // OSX attempt.
+  homeDir := os.Getenv("HOME")
+  configRoot := filepath.Join(homeDir, "Library", "Preferences")
+  _, err = os.Stat(filepath.Join(configRoot, "Blizzard", "Hearthstone"))
+  if homeDir != "" && err == nil  {
+    return filepath.Join(configRoot, "Blizzard", "Hearthstone", "log.config")
+  }
+
+  // Failed to find the default path.
+  return ""
 }
 
 // DefaultLogPath returns the path to Hearthstone's logging output file.
 //
 // It returns the expected file path, assuming a standard game installation.
 func DefaultLogFile() string {
-  if runtime.GOOS == "windows" {
-    var programFiles string
-    if runtime.GOARCH == "amd64" {
-      programFiles = filepath.Join("C:", "Program Files (x86)")
-    } else {
-      programFiles = filepath.Join("C:", "Program Files")
+  // Windows attempts.
+  for _, programDir := range []string{"Program Files (x86)", "Program Files"} {
+    dataDir := filepath.Join("C:", programDir, "Hearthstone",
+        "Hearthstone_data")
+    if _, err := os.Stat(dataDir); err == nil {
+      return filepath.Join(dataDir, "output_log.txt")
     }
-    return filepath.Join(programFiles, "Hearthstone", "Hearthstone_data",
-        "output_log.txt")
   }
 
-  // OSX.
-  return filepath.Join(os.Getenv("HOME"), "Library", "Logs", "Unity",
-      "Player.log")
+  // OSX attempt.
+  homeDir := os.Getenv("HOME")
+  _, err := os.Stat(filepath.Join(homeDir, "Library"))
+  if homeDir != "" && err == nil {
+    return filepath.Join(homeDir, "Library", "Logs", "Unity", "Player.log")
+  }
+
+  // Failed to find a default path.
+  return ""
 }
